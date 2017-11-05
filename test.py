0d3f34a82cf3e21120d55885535a26b75b284539
luyh <hebbelu@icloud.com>
2017-11-05 17:45:32 +0800

    更新获取现价，自动下单

--- test.py
+++ test.py
@@ -1,6 +1,9 @@
 import easytrader
 from hbyq import hbyq
 import time
+import win32api
+import win32con
+
 #导入网格
 wangge = hbyq.import_wg()
 
@@ -9,24 +12,34 @@ wangge = hbyq.import_wg()
 amount = 300
 
 #获取行情
-close = hbyq.close_price()
+now_pri = hbyq.now_price()
 
 
 # 获取网格所在位置
 (pos,pos_pri,up_pri,down_pri) = hbyq.position(close,wangge)
 
-print('close:',close,'pos:',pos,'pos_price',pos_pri, 'up:',up_pri,'down:',down_pri)
+print('now:',now_pri,'pos:',pos,'pos_price',pos_pri, 'up:',up_pri,'down:',down_pri)
 
 #设置easytrader
 user = easytrader.use('yh_client') # 银河客户端支持 ['yh_client', '银河客户端']
 user.prepare('yh.json') # 配置文件路径
 
+
 #买入卖出
 user.buy('162411',down_pri,amount)
-time.sleep(5)
+win32api.keybd_event(13,0,0,0)  #enter键位码是13
+win32api.keybd_event(13,0,win32con.KEYEVENTF_KEYUP,0) #释放按键
+time.sleep(0.1)
+win32api.keybd_event(13,0,0,0)  #enter键位码是13
+win32api.keybd_event(13,0,win32con.KEYEVENTF_KEYUP,0) #释放按键
+
 
 user.sell('162411',up_pri,amount)
-time.sleep(5)
+win32api.keybd_event(13,0,0,0)  #enter键位码是13
+win32api.keybd_event(13,0,win32con.KEYEVENTF_KEYUP,0) #释放按键
+time.sleep(0.1)
+win32api.keybd_event(13,0,0,0)  #enter键位码是13
+
 
 #完结
 print("ok")
