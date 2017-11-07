17d600f0f4cc1d371740bfd07aed16fd1790b346
luyh <hebbelu@icloud.com>
2017-11-07 21:59:01 +0800

    补充完3个网格，待测试

--- hbyq.py
+++ hbyq.py
@@ -8,16 +8,6 @@ import easytrader
 
 
 class hbyq:
-    def now_price():  # 获取华宝油气收盘价
-        quotation = easyquotation.use( 'sina' )  # 新浪 ['sina'] 腾讯 ['tencent', 'qq']
-
-        request =  quotation.real( '162411' )  # 支持直接指定前缀，如 'sh000001'
-        #print(request)
-        now_pri = request['162411']['now']  ## 收盘价
-
-        #print('now_pri:',now_pri)
-        #print(type(close))
-        return now_pri
 
     def position(now_pri,wangge):
         for i in range( len( wangge ) ):
--- test.py
+++ test.py
@@ -1,9 +1,9 @@
-import easytrader
+
 from hbyq import hbyq
 import time
 import pandas as pd
 
-trade = ('162411','512880','159915')
+stock = ['162411','512880','159915']
 #网格数据，参考numbers文件
 hbqy = (0.84,0.816,0.793,0.771,0.749,0.728,0.708,0.688,0.668,0.649,0.631,0.613,0.596,0.579,0.563,0.547,0.532,0.517,0.502,0.488,0.474)
 zqetf =(1.295,1.263,1.233,1.203,1.173,1.145,1.117,1.089,1.063,1.037,1.012,0.987,0.963,0.939,0.917,0.894)
@@ -17,7 +17,7 @@ d = {
 
 df = pd.DataFrame(d)
 
-# print(df)
+print(u'网格表:',df)
 
 
 # 设置网格交易量
@@ -26,7 +26,7 @@ amount = {
     '512880':600,
     '159915':200,
 }
-
+print('amount',amount)
 
 #获取行情
 import easyquotation
@@ -40,45 +40,76 @@ now_pri={
     '159915': 0,
 }
 
-for _trade in trade:
-    request = quotation.real( _trade )  # 支持直接指定前缀，如 'sh000001'
-    now_pri[_trade] = request[_trade]['now']  ## 收盘价
-    print(now_pri[_trade])
+
+request = quotation.real( stock )  # 支持直接指定前缀，如 'sh000001'
+
+for _stock in stock:
+    now_pri[_stock] = request[_stock]['now']  ## 收盘价
+
+print('now_pri',now_pri)
 
 
 # 获取网格所在位置
+
+position ={
+    '162411': 0,
+    '512880': 0,
+    '159915': 0,
+}
+
+for _stock in stock:
+    for pos in range( len( df[_stock] ) -1):
+        if now_pri[_stock] > df[_stock][pos]:
+            position[_stock] = pos
+            break
+
+print('position',position)
+
+# 下单网格价
+trade = {
+    '162411': {'buy':0,'sell':0},
+    '512880': {'buy':0,'sell':0},
+    '159915': {'buy':0,'sell':0},
+}
+
+for _stock in stock:
+    trade[_stock]['buy'] = ('%.3f' % df[_stock][position[_stock]+1])
+    trade[_stock]['sell'] = ('%.3f' % (df[_stock][position[_stock] - 1]))
+
+print('trade',trade)
+
 try:
-    (pos,pos_pri,up_pri,down_pri) = hbyq.position(now_pri,wangge)
-    print( 'now:', now_pri, 'pos:', pos, 'pos_price', pos_pri, 'up:', up_pri, 'down:', down_pri )
-    # 设置easytrader
-    user = easytrader.use( 'yh_client' )  # 银河客户端支持 ['yh_client', '银河客户端']
+    import easystockr
+    # 设置easystockr,需在windows下安装银河客户端，详见easystockr说明
+    user = easystockr.use( 'yh_client' )  # 银河客户端支持 ['yh_client', '银河客户端']
     user.prepare( 'yh.json' )  # 配置文件路径
-
-    try:
-        import win32api
-        import win32con
-
-    except(ImportError):
-        print( 'ModuleNotFoundError: No module named \'win32api\'' )
-
-    # 买入卖出
-    user.buy( '162411', down_pri, amount )
-    win32api.keybd_event( 13, 0, 0, 0 )  # enter键位码是13
-    win32api.keybd_event( 13, 0, win32con.KEYEVENTF_KEYUP, 0 )  # 释放按键
-    time.sleep( 0.1 )
-    win32api.keybd_event( 13, 0, 0, 0 )  # enter键位码是13
-    win32api.keybd_event( 13, 0, win32con.KEYEVENTF_KEYUP, 0 )  # 释放按键
-
-    user.sell( '162411', up_pri, amount )
-    win32api.keybd_event( 13, 0, 0, 0 )  # enter键位码是13
-    win32api.keybd_event( 13, 0, win32con.KEYEVENTF_KEYUP, 0 )  # 释放按键
-    time.sleep( 0.1 )
-    win32api.keybd_event( 13, 0, 0, 0 )  # enter键位码是13
-
 except:
-    print('error')
+    print(u'请检查客户端或easystockr设置')
 
 
-#完结
-print("ok")
+# 自动下单失灵，为下单，引入回车确认下单
+try:
+    import win32api
+    import win32con
+except(ImportError):
+    print(u'自动下单失灵，为下单，引入回车确认下单')
+    print( u'加载win32api异常：ModuleNotFoundError: No module named \'win32api\'' )
+
+def key_enter():
+    win32api.keybd_event( 13, 0, 0, 0 )  # enter键位码是13
+    win32api.keybd_event( 13, 0, win32con.KEYEVENTF_KEYUP, 0 )  # 释放按键
+    time.sleep( 0.2 )
+
+# 买入卖出
+try:
+    for _stock in stock:
+        user.buy( _stock, trade[_stock]['buy'], amount[_stock] )
+        key_enter()
+
+        user.sell( _stock, trade[_stock]['sell'], amount[_stock] )
+        key_enter()
+except:
+    print(u'下单异常')
+else:
+    print(u'网格已成功下单')
 
